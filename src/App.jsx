8f88328f5a02134749f270e1040936b452d40426
Josev.21 <jose.armando.vera.21@gmail.com>
2025-10-18 18:21:07 -0300

    Agregado del ReporteStock, actualiza routes y App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginForm from "./components/LoginForm";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import Home from "./components/Home";
 import ClientesPorCiudad from "./components/reportes/ClientesPorCiudad";
+import ReporteStock from "./components/reportes/ReporteStock";
 import AppLayout from "./components/layout/AppLayout";
 
 function App() {
@@ -28,6 +29,15 @@ function App() {
             </AppLayout>
           }
         />
+          <Route
+            path="/stock-por-producto"
+            element={
+              <AppLayout>
+                <ReporteStock />
+              </AppLayout>
+            }
+          />
+          
       </Route>
 
       <Route path="*" element={<AppLayout><h1>404 - Página no encontrada</h1></AppLayout>} />
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,7 +1,10 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export const CIUDADES_ENDPOINT = `${API_BASE_URL}/api/v1/location/ciudad`;
 export const REPORTES_CLIENTES_POR_CIUDAD_ENDPOINT = `${API_BASE_URL}/api/v1/reports/clientes_por_ciudad/`;
+
+export const PRODUCTOS_ENDPOINT = `${API_BASE_URL}/api/v1/producto/`;
+export const REPORTE_STOCK_DETALLE = `${API_BASE_URL}/api/v1/reports/reportes/producto/`;
 
 export const USER_ENDPOINT = `${API_BASE_URL}/api/v1/users/me`;
 export const LOGIN_ENDPOINT = `${API_BASE_URL}/api/v1/auth/token`;
